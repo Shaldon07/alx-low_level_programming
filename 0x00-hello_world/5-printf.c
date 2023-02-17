59540e3dabbc0cd3f57ad3bc1a9ef13df3520727
Shaldon Maarman <shaldonm7@gmail.com>
2023-02-17 05:48:03 -0800

    5.Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function of the code
+ *
+ * Return: zero when the code is sucessful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+

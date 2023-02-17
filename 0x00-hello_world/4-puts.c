33bbecb93ca85ecda92848eca9620100e0453e80
Shaldon Maarman <shaldonm7@gmail.com>
2023-02-17 05:39:03 -0800

    4.Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: always zero
+ */
+
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle\n");
-        return (0);	
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }

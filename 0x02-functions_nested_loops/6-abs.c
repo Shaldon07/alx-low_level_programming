d39142b622c6218fe8437f9ccf7e903dc326a3e9
Shaldon Maarman <shaldonm7@gmail.com>
2023-02-21 14:04:07 -0800

    6.Absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - checks for absolute value
+ * @a: parameters to be checked
+ * Return: ALways a
+ */
+
+int _abs(int a)
+{
+	if (a < 0)
+	{
+		a = -(a);
+	}
+	else if (a >= 0)
+	{
+		a = a;
+	}
+	return (a);
+}

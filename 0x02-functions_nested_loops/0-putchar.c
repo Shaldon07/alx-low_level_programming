3772e5c1de907b5ee16def82b5400069f3125561
Shaldon Maarman <shaldonm7@gmail.com>
2023-02-21 11:08:26 -0800

    Headerfile

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <main.h>
+/**
+ * main - main function and point of entry
+ * _putchar: prints the characters putchar
+ * Return: Returns 0 when successful
+ */
+
+int main(void)
+{
+	_putchar('\n');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}

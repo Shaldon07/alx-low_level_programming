77db541da7bdfc6ad050695037d914ced171249a
Shaldon Maarman <shaldonm7@gmail.com>
2023-06-07 05:01:16 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,16 +3,18 @@
 /**
  * _puts_recursion - similar function to puts();
  * @s: insertion
- * Return: 0 when successful
+ * Return: returns nothing
  */
 
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
 		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }

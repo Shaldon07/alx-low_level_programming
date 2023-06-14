fd1f9c6696fcc26cb618518130acc41890578264
Shaldon Maarman <shaldonm7@gmail.com>
2023-06-14 05:17:04 -0700

    Task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -35,40 +35,44 @@ int word_count(char *s)
  * Return: pointer to an array of strings when sucessful
  * and NULL when error
  */
+
 char **strtow(char *str)
 {
-
 	char **matrix, *tmp;
-	int x, y, z, ln = 0, wrds, start, end;
+	int i, k = 0, len = 0, words, c = 0, start, end;
 
-	while (*(str + ln))
-		ln++;
-
-	wrds = word_count(str);
-	if (wrds == 0)
+	while (*(str + len))
+		len++;
+	words = count_word(str);
+	if (words == 0)
 		return (NULL);
 
-	for (x = 0; x <= ln; x++)
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
+	if (matrix == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
 	{
-		if (str[x] == ' ' || str[x] == '\0')
+		if (str[i] == ' ' || str[i] == '\0')
 		{
-			if (z)
+			if (c)
 			{
-				end = x;
-				tmp = (char *) malloc(sizeof(char) * (z + 1));
+				end = i;
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
 				if (tmp == NULL)
 					return (NULL);
 				while (start < end)
 					*tmp++ = str[start++];
 				*tmp = '\0';
-				matrix[y] = tmp - z;
-				y++;
-				z = 0;
+				matrix[k] = tmp - c;
+				k++;
+				c = 0;
 			}
 		}
-	else if (z++ == 0)
-		start = x;
+		else if (c++ == 0)
+			start = i;
 	}
-	matrix[y] = NULL;
+
+	matrix[k] = NULL;
 	return (matrix);
 }
